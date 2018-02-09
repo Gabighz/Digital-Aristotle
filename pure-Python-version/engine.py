f18e649ed9366752855902f0af98c54dfc393580
Gabighz <ghiuzangabriel2@gmail.com>
2018-02-09 16:05:10 +0000

    Added simple tokenization and identification of intent

--- pure-Python-version/engine.py
+++ pure-Python-version/engine.py
@@ -3,3 +3,36 @@
 # Gabriel Ghiuzan (914067@swansea.ac.uk) & Slade Brooks (963336@swansea.ac.uk) (2018)
 #
 ##
+
+#string word;
+#while()
+#{
+#    word = checkValue(file line)
+#    if(word.equals("cs-150"))
+#    {
+#        while()
+#        {
+#            word = checkValue(file line)
+#        }
+#    }
+#}
+
+
+
+def classifier(input):
+    input_tokens = input.split()
+    print(input_tokens)
+
+    for word in input_tokens:
+        if word == "what":
+            intent = "what"
+        elif word == "how":
+            intent = "how"
+        else:
+            intent = "none"
+
+def main():
+    input = "what is a multiplexer?"
+    classifier(input)
+
+main()
