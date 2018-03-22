cae4d1a706ddffc662f00fcd4a968e5b29e2855f
localhost <ghiuzangabriel2@gmail.com>
2018-03-22 16:41:17 +0000

    Restructured the source files of the IR system

--- Information Retrieval System/src/KeywordExtractor.py
+++
@@ -1,7 +0,0 @@
-# XML parser - import dataset (Slade Brooks)
-
-# Feature assignment (Slade Brooks)
-
-# Perform K-means clustering (2 clusters, Keyword & Not-Keyword) (Gabriel Ghiuzan)
-
-# Write the contents of the classifications to a file (Gabriel Ghiuzan)
---
+++ Information Retrieval System/src/Main.py
@@ -0,0 +1,6 @@
+##################################################
+# Main program which executes the modules,       #
+# handles pre-processing and post-processing,    #
+# and writes Keywords and Non-keywords to a file #
+# (Gabriel Ghiuzan)                              #
+##################################################
