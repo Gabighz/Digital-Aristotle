5ed455bdcbc562afea79730f7dada3e9b571f705
LILIANA GABRIELA GHIUZAN <ghiuzangabriel2@gmail.com>
2018-03-27 18:42:43 +0100

    Initial K means clustering commit

--- Information Retrieval System/src/KMeans.py
+++ Information Retrieval System/src/KMeans.py
@@ -1,3 +1,39 @@
 ########################################################################################
 # Perform K-means clustering (2 clusters, Keyword & Not-Keyword) (Gabriel Ghiuzan)     #
 ########################################################################################
+
+MAX_ITERATIONS = 100
+
+# Import data and store in dataset
+def construct_dataset (wordList):
+
+    # Contains each word as a string and the sum of its features
+    dataset = []
+
+    for word in wordList:
+        for feature in range(1, len(word)):
+            featureSum += word[feature]
+
+        dataset.append([word[0], featureSum])
+
+# Euclidean Distance calculator in a one-dimensional space
+def euclidean_distance(p, q):
+    return math.abs(q - p)
+
+# Initialize centroids to the first elements in the dataset
+def initialize_centroids(k):
+
+    for i in range(k):
+        centroids[i] = dataset[i]
+
+    return centroids
+
+# Apply K-means clustering to dataset
+def kmeans_clustering(dataset, k):
+
+    centroids = initialize_centroids(k)
+    clusters = []
+
+    for i in range(MAX_ITERATIONS):
+
+        for observation in dataset:
