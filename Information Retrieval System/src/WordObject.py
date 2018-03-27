d31e71ef0c86eb579048d04c6adc2f9d11bac2b8
Slade-Brooks <36275265+Slade-Brooks@users.noreply.github.com>
2018-03-27 05:13:15 +0100

    Add files via upload

---
+++ Information Retrieval System/src/WordObject.py
@@ -0,0 +1,45 @@
+class WordObject:
+    #the variables that determine its order
+    documentNumber = 0
+    pageNumber = 0
+    sentenceNumber = 0
+    wordNumber = 0
+
+    #The on page attributes
+    top = 0
+    left = 0
+    width = 0
+    height = 0
+    fontSize = 0
+
+    #the word itself
+    wordString = ""
+    wordData = 0
+    isBold = 0
+
+    def __init__(self,word,isBold,documentNumber,pageNumber,sentenceNumber,wordNumber,top,left,width,height,fontSize):
+        self.isBold = isBold
+        self.wordString = word
+        self.documentNumber = documentNumber
+        self.pageNumber = pageNumber
+        self.sentenceNumber = sentenceNumber
+        self.wordNumber = wordNumber
+        self.top = top
+        self.left = left
+        self.width = width
+        self.height = height
+        self.fontSize = fontSize
+        self.wordData = 1
+        self.wordString = word
+
+    def returnWordArray():
+        output = [documentNumber,pageNumber,sentenceNumber,wordNumber,top,left,width,height,fontSize,wordData]
+        return output
+
+        #need to finnish
+    def setWordData():
+        return 1;
+
+    def wordInfo(self):
+        info = str(self.isBold) + " " + str(self.wordString) + " " + str(self.documentNumber) + str(self.pageNumber) + " " + str(self.sentenceNumber) + " " + str(self.wordNumber) + " " + str(self.top) + " " + str(self.left) + " " + str(self.height)+ " " + str(self.fontSize) + " " + str(self.wordData)
+        return info
--- Information Retrieval System/src/XMLParser.py
+++ Information Retrieval System/src/XMLParser.py
@@ -1,3 +1,78 @@
-##################################################
-# XML parser - import dataset (Slade Brooks)     #
-##################################################
+
+from xml.etree import ElementTree
+from WordObject import WordObject
+
+#
+#Variables
+#
+pageNumber = 0
+sentenceNumber = 0
+wordNumber = 0
+isBold = 0
+wordList = []
+
+#Parses words
+def hasChild(element):
+    if element.text is None:
+        return True
+
+    else:
+        return False
+
+def getAttribs(e):
+    attributes = [e.attrib['top'],e.attrib['left'],e.attrib['width'],e.attrib['height'],e.attrib['font']]
+    return attributes
+
+def parseWord(word,sentence):
+    global sentenceNumber
+    global pageNumber
+    global wordNumber
+    global isBold
+    global fontSize
+    global wordList
+    wordNumber += 1
+    attributes = getAttribs(sentence)
+    wordOb = WordObject(word,isBold,1,pageNumber,sentenceNumber,wordNumber,attributes[0],attributes[1],attributes[2],attributes[3],attributes[4])
+    wordList.append(wordOb)
+
+def parseWords(words,sentence):
+    for e in words:
+        parseWord(e,sentence)
+
+#Parses Sentences
+def parseSentences(sentences):
+    for d in sentences:
+        #global fontSize
+        #fontSize = d.attrib['font']
+        global sentenceNumber
+        sentenceNumber += 1
+        if not hasChild(d):
+            words = d.text.split()
+            parseWords(words,d)
+        else:
+            for child in d:
+                if child.tag == 'b':
+                    temp = child.text.strip()
+                    temp = temp.split()
+                    global isBold
+                    isBold = 1
+                    parseWords(temp,d)
+                    isBold = 0
+
+
+def parsePages(pages):
+    for c in pages:
+        global pageNumber
+        pageNumber += 1
+        sentences = c.findall('text')
+        parseSentences(sentences)
+
+def printWords(list):
+    for w in list:
+        print(w.wordInfo())
+
+
+dom = ElementTree.parse('Data.xml')
+pages = dom.findall('page')
+parsePages(pages)
+printWords(wordList)
