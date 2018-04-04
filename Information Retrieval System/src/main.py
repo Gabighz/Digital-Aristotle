8e29d5d776dc1b5b1587104d74361f748b6dce4f
localhost <ghiuzangabriel2@gmail.com>
2018-04-04 05:29:49 +0300

    Linked the individual modules in main.py

--- Information Retrieval System/src/main.py
+++ Information Retrieval System/src/main.py
@@ -7,14 +7,24 @@
 CLUSTERS = 2
 
 # to be changed with functions where necessary
-from .XMLParser import *
-from .FeatureSelection import *
-from .KMeans import *
+from XML_parser import *
+from feature_selection import *
+from kmeans import kmeans_clustering
 
 def main():
-    # Open a file here and apply a function from XMLParser to it
+    # Open a file here and apply a function from XML_parser to it
 
-    # Apply a function from FeatureSelection to the return of the aforementioned function.
+    path = '../input/foundation-year/CSC079/Slides-Week-2.xml' # to be changed to something more flexible in the future
+    parsed_files = XML_parser()
+    parsed_files.parse_file(path)
+
+    selected_features = feature_selection(parsed_files.get_parsed_words()) # to be changed to something pythonic
+
+    var = assignedFeatures.getWordList() # what is var? please use meaningful identifier names
+    for e in var: # what is e?
+        print(e)
+
+    # Apply a function from feature_selection to the return of the aforementioned function.
     # Return a two-dimensional array which contains each word and its features, named word_list
 
     # Apply K-means clustering to the aforementioned array
