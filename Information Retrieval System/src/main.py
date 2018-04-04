34161127b90ee17cbc417ab153adef3ba6666d5c
localhost <ghiuzangabriel2@gmail.com>
2018-04-04 05:36:32 +0300

    Commented main.py and applied style conventions

--- Information Retrieval System/src/main.py
+++ Information Retrieval System/src/main.py
@@ -18,14 +18,15 @@ def main():
     parsed_files = XML_parser()
     parsed_files.parse_file(path)
 
-    selected_features = feature_selection(parsed_files.get_parsed_words()) # to be changed to something pythonic
-
-    var = assignedFeatures.getWordList() # what is var? please use meaningful identifier names
-    for e in var: # what is e?
-        print(e)
-
     # Apply a function from feature_selection to the return of the aforementioned function.
     # Return a two-dimensional array which contains each word and its features, named word_list
+    # !! To be changed to a single 'interface' function, in a pythonic way
+
+    selected_features = feature_selection(parsed_files.get_parsed_words())
+
+    var = selected_features.get_word_list() # what is var? please use meaningful identifier names
+    for e in var: # what is e?
+        print(e)
 
     # Apply K-means clustering to the aforementioned array
     kmeans_clustering(word_list, CLUSTERS)
