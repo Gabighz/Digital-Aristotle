07291efcd8e886b1d8e25e4152cc6eb9423b7359
localhost <ghiuzangabriel2@gmail.com>
2018-04-24 15:15:22 +0100

    Initial processing commit

--- Information Retrieval System/src/main.py
+++ Information Retrieval System/src/main.py
@@ -1,6 +1,5 @@
 ##################################################
 # Main program which executes the modules,       #
-# handles pre-processing and post-processing,    #
 # and writes Keywords and Non-keywords to a file #
 ##################################################
 
---
+++ Information Retrieval System/src/processing.py
@@ -0,0 +1,8 @@
+##################################################
+# Handles pre-processing (stopwords filtering)   #
+# and post-processing (performance testing)      #
+##################################################
+
+def pre_processing():
+
+def post_processing():
