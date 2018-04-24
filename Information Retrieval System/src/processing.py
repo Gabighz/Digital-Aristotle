75e8b7c98d893a5740fc067823cb721249047d3a
Slade Brooks <slade-brooks@outlook.com>
2018-04-24 15:43:33 +0100

    added stop word removal to pre processing using nltk's stop word list

--- Information Retrieval System/src/processing.py
+++ Information Retrieval System/src/processing.py
@@ -2,7 +2,24 @@
 # Handles pre-processing (stopwords filtering)   #
 # and post-processing (performance testing)      #
 ##################################################
+from nltk.corpus import stopwords
 
-def pre_processing():
+#this function removes all stop word entries from the raw data from the xml_parser
+def pre_processing(raw_data):
+    word_data_array = raw_data
+    array_counter = 0
+    stop_words = stop_words.words("english")
+    #this loop runs through every word in the array of word data
+    for word_data in word_data_array:
+
+        #the below variable and loop are for running through each stop word for a match
+        stop_word_counter = 0
+        while word_data[array_counter] != stop_word
+            stop_word = stop_words[stop_word_counter]
+            if word_data[array_counter] == stop_word:
+                del word_data_array[array_counter]
+            stop_word_counter +=1
+        array_counter += 1
+        return word_data_array
 
 def post_processing():
