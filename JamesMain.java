5dde4cf2cbfc7c5cbd078129790004b1b1f1f540
Slade Brooks <slade-brooks@outlook.com>
2018-07-04 15:41:00 +0100

    atempting to fix bad merge

--- JamesMain.java
+++
@@ -1,9 +0,0 @@
-class JamesMain{
-    James firstJames = new James();
-
-    public static void main(String[] args){
-
-        System.out.println(firstJames.getEyeColour());
-        System.out.println(firstJames.toString())
-    }
-}
