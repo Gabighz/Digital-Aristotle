3df6b3fc04bc809dedf93184da1988553ad41d93
Gabriel Ghiuzan <ghiuzangabriel2@gmail.com>
2018-10-02 13:48:18 +0100

    Initial AIML generator commit

---
+++ Information Retrieval System/AIML generator/main.py
@@ -0,0 +1,7 @@
+##################################################
+# Main program which writes AIML patterns and    #
+# templates in .aiml format based on the outputs #
+# of the Keyword and Keyphrase extractors        #
+#                                                #
+# Author: Gabriel Ghiuzan                        #
+##################################################
