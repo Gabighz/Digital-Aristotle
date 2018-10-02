a021154b67462b74ab646ed6c7d31213d945d4ce
Gabriel Ghiuzan <ghiuzangabriel2@gmail.com>
2018-10-02 18:00:35 +0100

    Added ability to write AIML files with the name of the processed keyword files and with header information

--- information_retrieval_system/aiml_generator/main.py
+++
@@ -1,7 +0,0 @@
-##################################################
-# Main program which writes AIML patterns and    #
-# templates in .aiml format based on the outputs #
-# of the Keyword and Keyphrase extractors        #
-#                                                #
-# Author: Gabriel Ghiuzan                        #
-##################################################
