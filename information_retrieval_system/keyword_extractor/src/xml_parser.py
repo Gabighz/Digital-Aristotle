102d039fe610b0765bc2bbc8483087406fa9e1ce
Gabriel Ghiuzan <ghiuzangabriel2@gmail.com>
2018-10-04 20:33:05 +0100

    Added the ability to extract each word or sentence and its relevant XML attributes from the input file

--- information_retrieval_system/keyword_extractor/src/xml_parser.py
+++ information_retrieval_system/keyword_extractor/src/xml_parser.py
@@ -1,6 +1,6 @@
 ############################################################################
 # Takes lectures notes which were pre-converted from pdf or pptx into XML  #
-# and parses the XML attributes of each word or sentence                   #
+# and parses the relevant XML attributes of each word or sentence          #
 # Author: Gabriel Ghiuzan                                                  #
 ############################################################################
 
@@ -10,17 +10,49 @@ import xml.etree.ElementTree as elementTree
 # Creates a two-dimensional array which contains each word or sentence and its XML attributes
 #
 # @param path: The path of the XML file
-# @return parsed_xml: A two-dimensional array which contains each word or sentence and its XML attributes
+# @return parsed_xml: A two-dimensional array which contains each word or sentence and its relevant XML attributes.
+#                     The format is [word, is_bold, font_size, colour]
 def parse_xml(path):
 
+    # Parses the XML document
     tree = elementTree.parse(path)
 
+    # Gets the root of the XML element tree
     tree_root = tree.getroot()
 
+    # Will contain all fontspecs
+    # A fontspec specifies the size, family and colour of a font
     document_fontspecs = []
 
+    # Extracts all fontspecs from the document
     for fontspec in tree_root.iter('fontspec'):
         document_fontspecs.append(fontspec.attrib)
 
+    # Will contain each word or sentence and its XML features
+    parsed_xml = []
 
-    
+    # Extracts all text and their attributes
+    for text in tree.findall('.//page/text'):
+        # Will contain a word or sentence and its XML features
+        word = []
+
+        # Linear search to match text to its corresponding fontspec
+        for fontspec in document_fontspecs:
+            if text.attrib["font"] == fontspec["id"]:
+                size = fontspec["size"]
+                color = fontspec["color"]
+
+        # If tree.findall('.//page/text') returns a None object,
+        # it's because the word or sentence is contained by a bold or italic tag.
+        if text.text is None:
+
+            # Appends bolded text
+            for bolded in text.findall('b'):
+                word.extend((bolded.text, 1, size, color))
+
+        else:
+            word.extend((text.text, 0, size, color))
+
+        parsed_xml.extend([word])
+
+    print(parsed_xml)
