127f3fa6611ab11adab6d598586157100babf437
Gabighz <ghiuzangabriel2@gmail.com>
2018-10-26 10:42:46 +0300

    Rewrote the Keyphrase Extractor's reader of keywords in a much more concise way using numpy

--- information_retrieval_system/keyphrase_extractor/src/keyword_output.py
+++ information_retrieval_system/keyphrase_extractor/src/keyword_output.py
@@ -1,12 +1,15 @@
 ##################################################
 # Module which contains a function that stores   #
 # keywords into an array, given the output of the#
-# Keyword Extractor                              #
+# Keyword Extractor.                             #
+#                                                #
+# It is used as part of the input for the        #
+# Keyphrase Extractor.                           #
 #                                                #
 # Author: Gabriel Ghiuzan                        #
 ##################################################
 
-import re
+import numpy as np
 
 
 # Creates an array of keywords given the output of the Keyword extractor
@@ -17,26 +20,8 @@ def keyword_output(filename):
     # Concatenates the name of the file to a pre-determined input path
     path = "../../keyword_extractor/output/first-year/CS-150/" + filename
 
-    # Will contain keywords
-    keywords = []
-
-    # Extracts all keywords and ignores non-keywords line-by-line
-    #
-    # Each line of the file contains an array made up of a word and its keyword or non-keyword label,
-    # e.g. ['word', 1].
-    # However, these arrays are treated as strings given that they are stored in a .txt format.
-    with open(path, "r") as output_file:
-        for line in output_file:
-            # Cleans each line of non-alphanumeric characters
-            # However, it concatenates words with their keyword or non-keyword label
-            # e.g. ['word', 1] becomes word1
-            pattern = re.compile('[\W_]+')
-            filtered_line = pattern.sub('', line)
-
-            # Appends a word if it is a keyword, excluding its label
-            if filtered_line[-1] == "1":
-                keywords.append(filtered_line[:-1])
-
-    output_file.close()
+    keyword_extractor_output = np.loadtxt(open(path), delimiter=',', dtype=object)
+    print(keyword_extractor_output)
+    keywords = keyword_extractor_output[keyword_extractor_output[:, 1] == "1", 0]
 
     return keywords
