970375c9c603ab0493cb550c39909fd632bd752e
Gabighz <ghiuzangabriel2@gmail.com>
2019-04-06 18:57:28 +0100

    Moved all IR system files to one src directory

--- information_retrieval_system/src/aiml_generator.py
+++ information_retrieval_system/src/aiml_generator.py
@@ -7,8 +7,8 @@
 ##################################################
 
 import os
-from information_retrieval_system.keyphrase_extractor.src.main import keyword_output
-from information_retrieval_system.keyphrase_extractor.src.main import input_path
+from information_retrieval_system.src.keyphrase_extractor import keyword_output
+from information_retrieval_system.src.keyphrase_extractor import input_path
 
 
 def main():
