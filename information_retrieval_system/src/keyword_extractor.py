46bf3bcc6daebb7f7d49a223507d057f65d5d570
Gabighz <ghiuzangabriel2@gmail.com>
2019-04-07 13:51:54 +0100

    Redesigned the keyword extractor

--- information_retrieval_system/src/keyword_extractor.py
+++ information_retrieval_system/src/keyword_extractor.py
@@ -14,80 +14,71 @@ from processing import pre_processing, post_processing
 CLUSTERS = 2
 
 
-# This function requests user input of the name of a file as a string
+# Constructs the word list using the raw data which has been normalised, then set is filtered down to the values
+# within the variance threshold.
 #
-# @return filename: The name of the file which will be processed
-def input_path():
-    filename = input("Input path: ../../input/first-year/CS-150/")
+# @param input_file_path: The path of the XML input file. Used for XML parsing
+# @param filename: The name of the XML input file. Used for performance measuring in post processing
+#
+# @return keywords: An array of keywords extracted from the XML input file.
+def keyword_extractor(input_file_path, filename):
 
-    return filename
+	# An array which contains a phrase or a word and its corresponding XML data
+	parsed_xml = parse_xml(input_file_path)
 
+	# Prints the raw XML data
+	counter = 0
+	print("\n Raw XML Data: \n")
+	for word_array in parsed_xml:
+		print(word_array)
+		counter += 1
+	print("\n Number of word arrays in raw XML data: ", counter)
 
-def keyword_extractor():
-    # Opens a file and applies an XML parser to it. Can also handle the FileNotFoundError exception.
-    while True:
-        try:
-            # Gets the name of the file from the user
-            filename = input_path()
+	# Pre-processing to filter out unwanted data from parsed_content
+	filtered_content = pre_processing(parsed_xml)
 
-            # Concatenates the name of the file to a pre-determined input path
-            path = "../input/first-year/CS-150/" + filename + ".xml"
+	# Prints the filtered data so we can check if the pre-processor is working correctly
+	print("\n Filtered data (each word with its own XML features): \n")
+	counter = 0
+	for word_array in filtered_content:
+		print(word_array)
+		counter += 1
+	print("\n Number of word arrays in filtered data: ", counter)
 
-            # An array which contains a phrase or a word and its corresponding XML data
-            parsed_xml = parse_xml(path)
+	# A two-dimensional array which contains each word and its features
+	classification_features = feature_assignment(filtered_content)
 
-            break
-        except FileNotFoundError:
-            print("File not found! Try again.")
+	# Prints each word and its classification features and the number of all words in classification_features
+	counter = 0
+	print("\n \n Each word and its classification features: \n")
+	for word in classification_features:
+		# Format: [word, is_bold, is_larger, is_not_black, RAKE]
+		print(word)
+		counter += 1
 
-    # Prints the raw XML data
-    counter = 0
-    print("\n Raw XML Data: \n")
-    for word_array in parsed_xml:
-        print(word_array)
-        counter += 1
-    print("\n Number of word arrays in raw XML data: ", counter)
+	print("\n Number of words in classification_features: ", counter)
 
-    # Pre-processing to filter out unwanted data from parsed_content
-    filtered_content = pre_processing(parsed_xml)
+	# Runs the k-means algorithm 10 times, a number chosen arbitrarily.
+	# The cost function of this algorithm converges towards local minima, not guaranteed to find
+	# the global minimum. Thus, several restarts are needed.
+	runs = []
+	for n in range(10):
+		# A 2D array which contains each word and its label
+		# For example, [word, 0] or [word, 1], which means [word, Non-keyword] or [word, Keyword]
+		classified_words = kmeans_clustering(classification_features, CLUSTERS)
 
-    # Prints the filtered data so we can check if the pre-processor is working correctly
-    print("\n Filtered data (each word with its own XML features): \n")
-    counter = 0
-    for word_array in filtered_content:
-        print(word_array)
-        counter += 1
-    print("\n Number of word arrays in filtered data: ", counter)
+		# Post-processing to measure the performance of our keyword extractor
+		performance = post_processing(classified_words, filename)
 
-    # A two-dimensional array which contains each word and its features
-    classification_features = feature_assignment(filtered_content)
+		runs.append([classified_words, performance])
 
-    # Prints each word and its classification features and the number of all words in classification_features
-    counter = 0
-    print("\n \n Each word and its classification features: \n")
-    for word in classification_features:
-        # Format: [word, is_bold, is_larger, is_not_black, RAKE]
-        print(word)
-        counter += 1
+	# The highest performing classification is extracted.
+	classified_words, max_performance = max(runs, key=lambda array: array[1])
 
-    print("\n Number of words in classification_features: ", counter)
+	# F1 score, from 0 to 1
+	print("\n F1 score: ", max_performance)
 
-    # Runs the k-means algorithm 10 times, a number chosen arbitrarily.
-    # The cost function of this algorithm converges towards local minima, not guaranteed to find
-    # the global minimum. Thus, several restarts are needed.
-    runs = []
-    for n in range(10):
-        # A 2D array which contains each word and its label
-        # For example, [word, 0] or [word, 1], which means [word, Non-keyword] or [word, Keyword]
-        classified_words = kmeans_clustering(classification_features, CLUSTERS)
+	# Takes only the keywords (labelled with 1)
+	keywords = classified_words[classified_words[:, 1] == "1", 0]
 
-        # Post-processing to measure the performance of our keyword extractor
-        performance = post_processing(classified_words, filename)
-
-        runs.append([classified_words, performance])
-
-    # The highest performing classification is extracted.
-    classified_words, max_performance = max(runs, key=lambda array: array[1])
-
-    # F1 score, from 0 to 1
-    print("\n F1 score: ", max_performance)
+	return keywords
