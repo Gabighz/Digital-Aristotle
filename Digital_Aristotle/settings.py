e25fca43dbc212b5bdec31bf32012acc01ccd160
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-05 23:00:43 +0100

    Changed DB Engine to PostgreSQL

--- Digital_Aristotle/settings.py
+++ Digital_Aristotle/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'Digital_Aristotle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatbot',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/localhost/Projects/Digital-Aristotle/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
