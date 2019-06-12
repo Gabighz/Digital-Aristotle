ba1bf52362649661758f4cadd6979124fe307c2d
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-12 22:55:49 +0100

    Initial end-to-end testing for chatbot app

---
+++ tests/chatbot/tests-view.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexViewTests(TestCase):
+    def test_no_chat(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
