d2219a0555aa77ebe7663ae6193e7a5472482f68
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-14 20:23:20 +0100

    Added login/logout functionality

--- apps/admin_area/views.py
+++ apps/admin_area/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/accounts/login/')
 def index(request, template_name="admin_area/index.html"):
     context = {'title': 'Digital Aristotle'}
     return render_to_response(template_name, context)
