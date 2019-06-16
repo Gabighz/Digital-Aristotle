f0618be0007e7db2d1db9674e86bc1b9db6cfcd0
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-16 20:52:29 +0100

    Initial files for file uploading

--- apps/admin_area/tests.py
+++ apps/admin_area/tests.py
@@ -3,20 +3,34 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-class IndexViewTests(TestCase):
+class AdminAreaViewTests(TestCase):
     def setUp(self):
         # Sets up the necessary objects for testing authentication and permissions
         self.user = User.objects.create_superuser("admin", "admin@admin.com", "password")
         self.client = Client()
 
-    def test_security_no_login(self):
+    def test_security_no_login_index(self):
         # If the user is not authenticated, they should receive a 302 (redirect) status
         response = self.client.get(reverse('admin_area:index'))
         self.assertEqual(response.status_code, 302)
 
-    def test_access_page(self):
+    def test_security_no_login_upload(self):
+        # If the user is not authenticated, they should receive a 302 (redirect) status
+        response = self.client.get(reverse('admin_area:upload'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_access_page_index(self):
         # If the user is authenticated, they should be able to access the index of the admin area
         self.client.login(username="admin", password="password")
 
         response = self.client.get(reverse('admin_area:index'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<a href="%s">Logout</a>' % reverse("logout"), html=True)
+
+    def test_access_page_upload(self):
+        # If the user is authenticated, they should be able to access the index of the admin area
+        self.client.login(username="admin", password="password")
+
+        response = self.client.get(reverse('admin_area:upload'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<a href="%s">Logout</a>' % reverse("logout"), html=True)
--- apps/admin_area/views.py
+++ apps/admin_area/views.py
@@ -6,3 +6,9 @@ from django.contrib.auth.decorators import login_required
 def index(request, template_name="admin_area/index.html"):
     context = {'title': 'Digital Aristotle'}
     return render_to_response(template_name, context)
+
+
+@login_required(login_url='/accounts/login/')
+def upload(request, template_name="admin_area/upload.html"):
+    context = {'title': 'Digital Aristotle'}
+    return render_to_response(template_name, context)
