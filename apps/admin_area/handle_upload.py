1f024507d6f5a0833a20be292e6d6acd31b54d79
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-19 04:46:23 +0100

    Separate module for handle_uploaded_file

---
+++ apps/admin_area/handle_upload.py
@@ -0,0 +1,23 @@
+import os
+
+from information_retrieval_system.convert import convert_to_pdf
+
+
+def handle_uploaded_file(file, filename, file_type):
+    path_upload = "information_retrieval_system/uploaded_files/" + filename
+
+    # Enables us to create directories from within the program
+    os.makedirs(os.path.dirname(path_upload), exist_ok=True)
+
+    with open(path_upload, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+    if file_type == 'application/pdf':
+        convert_to_pdf('uploaded_files/' + filename, filename)
+
+    elif file_type == 'application/pptx':
+        return 0  # to be changed
+
+    else:
+        print("File not supported")  # to be changed
--- apps/admin_area/views.py
+++ apps/admin_area/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import UploadFileForm
-import os
-
-from information_retrieval_system.convert import convert_to_pdf
+from .handle_upload import handle_uploaded_file
 
 
 @login_required(login_url='/accounts/login/')
@@ -24,24 +22,4 @@ def upload(request, template_name="admin_area/upload.html"):
         form = UploadFileForm()
 
     return render(request, template_name, context, {'form': form})
-
-
-def handle_uploaded_file(file, filename, file_type):
-    path_upload = "information_retrieval_system/uploaded_files/" + filename
-
-    # Enables us to create directories from within the program
-    os.makedirs(os.path.dirname(path_upload), exist_ok=True)
-
-    with open(path_upload, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-
-    if file_type == 'application/pdf':
-        convert_to_pdf('uploaded_files/' + filename, filename)
-
-    elif file_type == 'application/pptx':
-        return 0  # to be changed
-
-    else:
-        print("File not supported")  # to be changed
 
