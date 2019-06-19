021534701ff7eedbc9c4178cf11918ea7752b33b
Gabighz <ghiuzangabriel2@gmail.com>
2019-06-19 23:56:21 +0100

    Handling unsupported file types

--- apps/admin_area/handle_upload.py
+++ apps/admin_area/handle_upload.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import SuspiciousOperation
 import os
 
 from information_retrieval_system.convert import convert_to_pdf
@@ -9,15 +10,20 @@ def handle_uploaded_file(file, filename, file_type):
     # Enables us to create directories from within the program
     os.makedirs(os.path.dirname(path_upload), exist_ok=True)
 
-    with open(path_upload, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
+    if file_type in ['application/pdf', 'application/pptx']:
+
+        with open(path_upload, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+    else:
+        raise SuspiciousOperation(
+            'File type not supported'
+        )
 
     if file_type == 'application/pdf':
         convert_to_pdf(path_upload, filename)
 
-    elif file_type == 'application/pptx':
-        return 0  # to be changed
-
     else:
-        print("File not supported")  # to be changed
+        return 0  # to be changed with converter from pptx to xml
+
