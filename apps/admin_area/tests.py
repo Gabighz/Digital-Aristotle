66d8c2a2298f302ea6458f1fa970057e0e9329f3
Gabighz <ghiuzangabriel2@gmail.com>
2019-08-14 07:46:12 +0100

    File upload test

--- apps/admin_area/tests.py
+++ apps/admin_area/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
 
 
 class AdminAreaViewTests(TestCase):
@@ -29,10 +31,24 @@ class AdminAreaViewTests(TestCase):
                             % reverse("logout"), html=True)
 
     def test_access_page_upload(self):
-        # If the user is authenticated, they should be able to access the index of the admin area
+        # If the user is authenticated, they should be able to access the upload page of the admin area
         self.client.login(username="admin", password="password")
 
         response = self.client.get(reverse('admin_area:upload'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<a class="btn btn-light action-button" role="button" href="%s">Logout</a>'
                             % reverse("logout"), html=True)
+
+    def test_file_upload_and_conversion(self):
+        # Test whether lecture notes are successfully uploaded and stored on the system
+        self.client.login(username="admin", password="password")
+        test_file_path = "information_retrieval_system/test_input/ComputingComponents.pdf"
+
+        file = SimpleUploadedFile(test_file_path, b"file_content", content_type="application/pdf")
+        self.client.post(reverse('admin_area:upload'), {'file': file})
+
+        uploaded_file_path = "information_retrieval_system/uploaded_files/ComputingComponents.pdf"
+        converted_file_path = "information_retrieval_system/xml_files/ComputingComponents.xml"
+
+        assert os.path.isfile(uploaded_file_path)
+        assert os.path.isfile(converted_file_path)
