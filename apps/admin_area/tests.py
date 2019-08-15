17b29185a3a7065acfa44b0ec5ebbeb18e4dce86
Gabighz <ghiuzangabriel2@gmail.com>
2019-08-15 19:19:21 +0100

    Updated travis config

--- apps/admin_area/tests.py
+++ apps/admin_area/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.core.files.uploadedfile import UploadedFile
 import os
 
 
